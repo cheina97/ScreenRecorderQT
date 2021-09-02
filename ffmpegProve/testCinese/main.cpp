45665e95c76de39824d6f0bc84857b2224e1b912
cheina97 <cheinasso.francesco@gmail.com>
2021-09-02 22:24:12 +0200

    refactoring

--- ffmpegProve/testCinese/main.cpp
+++ ffmpegProve/testCinese/main.cpp
@@ -9,39 +9,50 @@ extern "C" {
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
-    //PART1 INIT
+AVFormatContext *pFormatCtx;
+AVCodecContext *pCodecCtx;
+AVCodec *pCodec;
+AVDictionary *options = NULL;
 
-    AVFormatContext *pFormatCtx;
+typedef struct {
+    int screen_number;
+    int x_offset;
+    int y_offset;
+    int width;
+    int height;
+} X11_GrabParameters;
+
+void initDataStructures(X11_GrabParameters x11gp) {
+    cout << string(getenv("DISPLAY")) + "." + to_string(x11gp.screen_number) + "+" + to_string(x11gp.x_offset) + "," + to_string(x11gp.y_offset) << endl;
     int i, videoindex;
-    AVCodecContext *pCodecCtx;
-    AVCodec *pCodec;
 
     pFormatCtx = avformat_alloc_context();
 
     avdevice_register_all();
 
-    AVDictionary *options = NULL;
-
     if (av_dict_set(&options, "framerate", "30", 0) < 0) {
         cout << "\nerror in setting framerate value" << endl;
         exit(1);
     }
-    if (av_dict_set(&options, "video_size", "640x480", 0) < 0) {
+    if (av_dict_set(&options, "video_size", (to_string(x11gp.width) + "x" + to_string(x11gp.height)).c_str(), 0) < 0) {
         cout << "\nerror in setting video_size" << endl;
         exit(1);
     }
 
-    //TODO parametrizzare
+    if (av_dict_set(&options, "preset", "medium", 0) < 0) {
+        printf("\nerror in setting preset values");
+        exit(1);
+    }
+
     AVInputFormat *ifmt = av_find_input_format("x11grab");
-    if (avformat_open_input(&pFormatCtx, ":1.0+0,0", ifmt, &options) != 0) {
+    if (avformat_open_input(&pFormatCtx, (string(getenv("DISPLAY")) + "." + to_string(x11gp.screen_number) + "+" + to_string(x11gp.x_offset) + "," + to_string(x11gp.y_offset)).c_str(), ifmt, &options) != 0) {
         cout << "Couldn't open input stream." << endl;
-        return -1;
+        exit(-1);
     }
 
     if (avformat_find_stream_info(pFormatCtx, NULL) < 0) {
         cout << "Couldn't find stream information." << endl;
-        return -1;
+        exit(-1);
     }
 
     videoindex = -1;
@@ -51,27 +62,34 @@ int main(int argc, char const *argv[]) {
         }
     if (videoindex == -1) {
         cout << "Didn't find a video stream." << endl;
-        return -1;
+        exit(-1);
     }
 
     //deprecato: pCodecCtx = pFormatCtx->streams[videoindex]->codec;
     pCodecCtx = avcodec_alloc_context3(NULL);
-    avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[videoindex]->codecpar );
-    
+    avcodec_parameters_to_context(pCodecCtx, pFormatCtx->streams[videoindex]->codecpar);
+
     pCodec = avcodec_find_decoder(pCodecCtx->codec_id);
     if (pCodec == NULL) {
         cout << "Codec not found."
              << endl;
-        return -1;
+        exit(-1);
     }
-    if(avcodec_open2(pCodecCtx, pCodec,NULL)<0)
-	{
-		printf("Could not open codec.\n");
-		return -1;
-	}
+    if (avcodec_open2(pCodecCtx, pCodec, NULL) < 0) {
+        printf("Could not open codec.\n");
+        exit(-1);
+    }
 
-    //FINE INIT CAMERA
-    cout<<"open camera ok"<<endl;
-    //INIZIO INIT CODEC
+    cout << "Data structures inited" << endl;
+}
+
+int main(int argc, char const *argv[]) {
+    X11_GrabParameters x11gp;
+    x11gp.screen_number = 0;
+    x11gp.x_offset = 0;
+    x11gp.y_offset = 0;
+    x11gp.height = 1080;
+    x11gp.width = 1920;
+    initDataStructures(x11gp);
     return 0;
 }
