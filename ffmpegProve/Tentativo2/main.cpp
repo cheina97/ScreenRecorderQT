5f18bb6122fcafc087cb563beed9ed562e33af4f
cheina97 <cheinasso.francesco@gmail.com>
2021-09-05 14:00:37 +0200

    increased performance

--- ffmpegProve/Tentativo2/main.cpp
+++ ffmpegProve/Tentativo2/main.cpp
@@ -1,5 +1,6 @@
 #include <time.h>
 
+#include <chrono>
 #include <iostream>
 #include <queue>
 
@@ -17,6 +18,20 @@ extern "C" {
 
 using namespace std;
 
+//speed measures
+int readframe_clock = 0;
+int readframe_clock_start = 0;
+int readframe_clock_end = 0;
+int decode_clock = 0;
+int decode_clock_start = 0;
+int decode_clock_end = 0;
+int scaling_clock = 0;
+int scaling_clock_start = 0;
+int scaling_clock_end = 0;
+int encode_clock = 0;
+int encode_clock_start = 0;
+int encode_clock_end = 0;
+
 AVFormatContext *avFmtCtx = NULL;
 AVCodecContext *avRawCodecCtx = NULL;
 AVCodecContext *avEncoderCtx;
@@ -24,7 +39,7 @@ AVDictionary *avRawOptions = NULL;
 AVCodec *avDecodec = NULL;
 AVCodec *avEncodec = NULL;
 struct SwsContext *swsCtx = NULL;
-AVPacket *avRawPkt = NULL;
+queue<AVPacket *> avRawPkt_queue;
 int videoIndex = -1;
 AVFrame *avYUVFrame = NULL;
 int width;
@@ -42,7 +57,6 @@ void initScreenSource(X11parameters x11pmt, bool fullscreen) {
     avdevice_register_all();
 
     avFmtCtx = avformat_alloc_context();
-    avRawPkt = (AVPacket *)malloc(sizeof(AVPacket));
     char *displayName = getenv("DISPLAY");
 
     if (fullscreen) {
@@ -76,29 +90,33 @@ void initScreenSource(X11parameters x11pmt, bool fullscreen) {
     AVInputFormat *avInputFmt = av_find_input_format("x11grab");
 
     if (avInputFmt == NULL) {
-        printf("av_find_input_format not found......\n");
+        cout << "av_find_input_format not found......"
+             << endl;
         exit(-1);
     }
 
     if (avformat_open_input(&avFmtCtx, (string(displayName) + "." + to_string(x11pmt.screen_number) + "+" + to_string(x11pmt.offset_x) + "," + to_string(x11pmt.offset_y)).c_str(), avInputFmt, &avRawOptions) != 0) {
-        printf("Couldn't open input stream.\n");
+        cout << "Couldn't open input stream."
+             << endl;
         exit(-1);
     }
 
     if (avformat_find_stream_info(avFmtCtx, &avRawOptions) < 0) {
-        printf("Couldn't find stream information.\n");
+        cout << "Couldn't find stream information."
+             << endl;
         exit(-1);
     }
 
-    for (int i = 0; i < avFmtCtx->nb_streams; ++i) {
+    for (int i = 0; i < (int)avFmtCtx->nb_streams; ++i) {
         if (avFmtCtx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             videoIndex = i;
             break;
         }
     }
 
-    if (videoIndex == -1 || videoIndex >= avFmtCtx->nb_streams) {
-        printf("Didn't find a video stream.\n");
+    if (videoIndex == -1 || videoIndex >= (int)avFmtCtx->nb_streams) {
+        cout << "Didn't find a video stream."
+             << endl;
         exit(-1);
     }
 
@@ -108,11 +126,13 @@ void initScreenSource(X11parameters x11pmt, bool fullscreen) {
 
     avDecodec = avcodec_find_decoder(avRawCodecCtx->codec_id);
     if (avDecodec == NULL) {
-        printf("Codec not found.\n");
+        cout << "Codec not found."
+             << endl;
         exit(-1);
     }
     if (avcodec_open2(avRawCodecCtx, avDecodec, NULL) < 0) {
-        printf("Could not open decodec . \n");
+        cout << "Could not open decodec . "
+             << endl;
         exit(-1);
     }
 
@@ -138,7 +158,8 @@ void initScreenSource(X11parameters x11pmt, bool fullscreen) {
 
     avEncodec = avcodec_find_encoder(AV_CODEC_ID_H264);
     if (!avEncodec) {
-        printf("h264 codec not found\n");
+        cout << "h264 codec not found"
+             << endl;
         exit(-1);
     }
 
@@ -163,14 +184,38 @@ void initScreenSource(X11parameters x11pmt, bool fullscreen) {
     av_dict_set(&param, "tune", "zerolatency", 0);  //实现实时编码
 
     if (avcodec_open2(avEncoderCtx, avEncodec, &param) < 0) {
-        printf("Failed to open video encoder!\n");
+        cout << "Failed to open video encoder!"
+             << endl;
         exit(-1);
     }
 }
 
-void captureStart(int frameNumber) {
+void getRawPackets(int frameNumber) {
+    cout << "Capturing from x11..." << endl;
+    readframe_clock_start = clock();
+    auto begin = std::chrono::high_resolution_clock::now();
+    AVPacket *avRawPkt;
+    for (int i = 0; i < frameNumber; i++) {
+        avRawPkt = (AVPacket *)malloc(sizeof(AVPacket));
+        avRawPkt_queue.push(avRawPkt);
+        if (av_read_frame(avFmtCtx, avRawPkt) < 0) {
+            cout << "Error in getting RawPacket from x11" << endl;
+        } else {
+            //cout << "Captured " << avRawPkt_queue.size() << " raw packets" << endl;
+        }
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    readframe_clock_end = clock();
+    readframe_clock += (readframe_clock_end - readframe_clock_start);
+    cout << "Capturing ended successfully, captured " << avRawPkt_queue.size() << "raw packets in " << elapsed.count() * 1e-9 << " seconds" << endl
+         << endl;
+}
+
+void decodeAndEncode(void) {
+    cout << "Decoding and encoding raw packets ..." << endl;
+
     int got_picture = 0;
-    int ret = 0;
     int flag = 0;
     int bufLen = 0;
     uint8_t *outBuf = NULL;
@@ -188,71 +233,61 @@ void captureStart(int frameNumber) {
     memset(&pkt, 0, sizeof(AVPacket));
     av_init_packet(&pkt);
 
-    unsigned int pkSn = 0;
     FILE *h264Fp = fopen("out.264", "wb");
 
-    int start_clock = clock();
-    int readframe_clock = 0;
-    int readframe_clock_start = 0;
-    int readframe_clock_end = 0;
-    int decode_clock = 0;
-    int decode_clock_start = 0;
-    int decode_clock_end = 0;
-    int encode_clock = 0;
-    int encode_clock_start = 0;
-    int encode_clock_end = 0;
-
+    AVPacket *avRawPkt;
     int i = 0;
-    while (i < frameNumber) {
-        readframe_clock_start = clock();
-        if (av_read_frame(avFmtCtx, avRawPkt) >= 0) {
-            readframe_clock_end = clock();
-            readframe_clock += (readframe_clock_end - readframe_clock_start);
-            if (avRawPkt->stream_index == videoIndex) {
-                //Inizio DECODING
-                //deprecato: flag = avcodec_decode_video2(avRawCodecCtx, avOutFrame, &got_picture, avRawPkt);
-                decode_clock_start = clock();
-                flag = avcodec_send_packet(avRawCodecCtx, avRawPkt);
-                if (flag < 0) {
-                    printf("Errore Decoding");
-                    break;
-                }
-                got_picture = avcodec_receive_frame(avRawCodecCtx, avOutFrame);
-                decode_clock_end = clock();
-                decode_clock += (decode_clock_end - decode_clock_start);
-                //Fine DECODING
-                if (got_picture == 0) {
-                    sws_scale(swsCtx, avOutFrame->data, avOutFrame->linesize, 0, avRawCodecCtx->height, avYUVFrame->data, avYUVFrame->linesize);
+    while (!avRawPkt_queue.empty()) {
+        //cout << "Remaining " << avRawPkt_queue.size() << " frames" << endl;
+        avRawPkt = avRawPkt_queue.front();
+        avRawPkt_queue.pop();
+        if (avRawPkt->stream_index == videoIndex) {
+            //Inizio DECODING
+            //deprecato: flag = avcodec_decode_video2(avRawCodecCtx, avOutFrame, &got_picture, avRawPkt);
+            decode_clock_start = clock();
+            flag = avcodec_send_packet(avRawCodecCtx, avRawPkt);
+            if (flag < 0) {
+                cout << "Errore Decoding: sending packet" << endl;
+            }
+            got_picture = avcodec_receive_frame(avRawCodecCtx, avOutFrame);
+            decode_clock_end = clock();
+            decode_clock += (decode_clock_end - decode_clock_start);
+            //Fine DECODING
+            if (got_picture == 0) {
+                scaling_clock_start = clock();
+                sws_scale(swsCtx, avOutFrame->data, avOutFrame->linesize, 0, avRawCodecCtx->height, avYUVFrame->data, avYUVFrame->linesize);
+                scaling_clock_end = clock();
+                scaling_clock += (scaling_clock_end - scaling_clock_start);
 
-                    got_picture = -1;  //forse non serve
-                    //Inizio ENCODING
-                    //deprecato: flag = avcodec_encode_video2(avEncoderCtx, &pkt, avYUVFrame, &got_picture);
-                    encode_clock_start = clock();
-                    flag = avcodec_send_frame(avEncoderCtx, avYUVFrame);
-                    got_picture = avcodec_receive_packet(avEncoderCtx, &pkt);
-                    encode_clock_end = clock();
-                    encode_clock += (encode_clock_end - encode_clock_start);
-                    //Fine ENCODING
+                //got_picture = -1;  //forse non serve
+                //Inizio ENCODING
+                //deprecato: flag = avcodec_encode_video2(avEncoderCtx, &pkt, avYUVFrame, &got_picture);
+                encode_clock_start = clock();
+                flag = avcodec_send_frame(avEncoderCtx, avYUVFrame);
+                got_picture = avcodec_receive_packet(avEncoderCtx, &pkt);
+                encode_clock_end = clock();
+                encode_clock += (encode_clock_end - encode_clock_start);
+                //Fine ENCODING
 
-                    if (flag >= 0) {
-                        if (got_picture == 0) {
-                            int w = fwrite(pkt.data, 1, pkt.size, h264Fp);
-                            cout << "Captured " << ++i << " frames" << endl;
+                if (flag >= 0) {
+                    if (got_picture == 0) {
+                        int w = fwrite(pkt.data, 1, pkt.size, h264Fp);
+                        if (w != pkt.size) {
+                            cout << "Error in writing file" << endl;
                         }
+                        //cout << "Elaborated " << ++i << " frames" << endl;
                     }
-                    av_packet_unref(&pkt);
-                    av_packet_unref(avRawPkt);
                 }
+                av_packet_unref(&pkt);
+                av_packet_unref(avRawPkt);
+            } else {
+                cout << "Errore Decoding: receiving packet " << got_picture << endl;
             }
         }
     }
-    fclose(h264Fp);
-    cout << "Finished" << endl
+    cout << "Decoded and Encoded successfully" << endl
          << endl;
-    int total_clock = readframe_clock + encode_clock + decode_clock;
-    cout << "ReadFrame clock: " << readframe_clock << " " << (float)100 * readframe_clock / total_clock << "%" << endl;
-    cout << "Decode clock: " << decode_clock << " " << (float)100 * decode_clock / total_clock << "%" << endl;
-    cout << "Encode clock: " << encode_clock << " " << (float)100 * encode_clock / total_clock << "%" << endl;
+    fclose(h264Fp);
 }
 
 int main(int argc, char const *argv[]) {
@@ -263,6 +298,16 @@ int main(int argc, char const *argv[]) {
     x11pmt.offset_y = 0;
     x11pmt.screen_number = 0;
     initScreenSource(x11pmt, false);
-    captureStart(30 * 180);
+    getRawPackets(30*60*30);
+    //decodeAndEncode();
+
+    cout << "Finished" << endl
+         << endl;
+    int total_clock = readframe_clock + encode_clock + decode_clock + scaling_clock;
+    cout << "ReadFrame clock: " << readframe_clock << " " << (float)100 * readframe_clock / total_clock << "%" << endl;
+    cout << "Decode clock: " << decode_clock << " " << (float)100 * decode_clock / total_clock << "%" << endl;
+    cout << "Scaling clock: " << scaling_clock << " " << (float)100 * scaling_clock / total_clock << "%" << endl;
+    cout << "Encode clock: " << encode_clock << " " << (float)100 * encode_clock / total_clock << "%" << endl;
+
     return 0;
 }
