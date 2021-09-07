8dd57dbe1f113002ca592898985e5eba69b58ae4
alessandracomparetto <alessandra.comparetto@gmail.com>
2021-09-07 20:37:50 +0200

    selezione area, passo 1

--- TestScreenDer/TestScreenDer/mainwindow.h
+++ TestScreenDer/TestScreenDer/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "AreaSelector.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,7 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    AreaSelector *areaSelector;
 
 private slots:
     void on_pushButtonSelectArea_clicked();
