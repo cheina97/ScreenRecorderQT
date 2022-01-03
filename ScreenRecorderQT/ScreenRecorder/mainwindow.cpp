2937d232e4a442e3cdeb712656009db4227a7525
Francesco Cheinasso <cheinasso.francesco@gmail.com>
2022-01-03 12:09:50 +0100

    Release 1.1.0

--- ScreenRecorderQT/ScreenRecorder/mainwindow.cpp
+++ ScreenRecorderQT/ScreenRecorder/mainwindow.cpp
@@ -402,7 +402,9 @@ void MainWindow::on_pushButtonStart_clicked() {
                 std::cout << "-> RECORDING..." << std::endl;
                 screenRecorder->record();
                 QMessageBox::information(this, tr("Recording ended"),
-                                         tr("The video have been recorded successfully.\n"));
+                                         tr(("The video have been recorded successfully.\n"
+                                            "The file has been saved at " +
+                                            outFilePath).c_str()));
             } catch (const std::exception &e) {
                 setGeneralDefaultProperties();
                 alignValues();
